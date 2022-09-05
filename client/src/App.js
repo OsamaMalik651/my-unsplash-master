57c079cd1c21d017495df89b20e3dbf8d9c4ba52
OsamaMalik651 <osama.malik013@gmai.com>
2022-09-05 16:09:23 -0600

    completed Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react';
+import Navbar from 'components/Navbar/index';
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      My Unsplash
+    <div className={styles.App}>
+      <Navbar />
     </div>
   );
 }
